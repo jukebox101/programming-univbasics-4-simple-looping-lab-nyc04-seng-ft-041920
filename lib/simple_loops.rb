2a41a5433ff215bf77cd5bcc3da717769d9f6a04
Leonora Squires <leonora.squires@gmail.com>
2020-04-23 18:52:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,9 +17,9 @@ end
 
 def return_string_array(array)
   count = 0
-  new_array = []
+  new_arr = []
   while count < array.length do
-    new_array.push(array[count].to_s)
+    new_arr.push(array[count].to_s)
     count += 1
   end
   new_array
